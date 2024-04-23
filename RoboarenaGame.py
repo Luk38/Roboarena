e65db3793eb8cc3f07fd14d3ebb61d7d94b477bd
Edw1n777 <edhoffmann511@gmail.com>
2024-04-24 00:18:08 +0200

    added a Start-Button

--- RoboarenaGame.py
+++ RoboarenaGame.py
@@ -2,9 +2,28 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
+# Define Screen and Colors
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+PURPLE = (128, 0, 128)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
+
+#Button
+BUTTON_WIDTH = 200
+BUTTON_HEIGHT = 100
+BUTTON_COLOR = "white"
+BUTTON_TEXT = "Start Game"
+BUTTON_TEXT_COLOR = "purple"
+
+#Font
+font = pygame.font.Font(None, 36) 
+text_surface = font.render(BUTTON_TEXT, True, BUTTON_TEXT_COLOR)
+text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
 
 while True:
     # Process player inputs.
@@ -20,6 +39,11 @@ while True:
 
     # Render the graphics here.
     # ...
+
+    # Draw Start-Button
+    pygame.draw.rect(screen, BUTTON_COLOR, (
+    SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2, SCREEN_HEIGHT // 2 - BUTTON_HEIGHT // 2, BUTTON_WIDTH, BUTTON_HEIGHT))
+    screen.blit(text_surface, text_rect)
 
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(60)         # wait until next frame (at 60 FPS)
