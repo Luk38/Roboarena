5f7a5e5705e84a22d8b944d7c3adc573c3e06903
Edw1n777 <edhoffmann511@gmail.com>
2024-05-29 01:37:49 +0200

    load a map feature added

--- arena.py
+++ arena.py
@@ -2,43 +2,37 @@ import pygame
 
 
 class arena:
-    def __init__(self, height, width, tileheight, tilewidth):
+    def __init__(self, height, width, tileheight, tilewidth, filename):
         self.height = height
         self.width = width
         self.tileheight = tileheight
         self.tilewidth = tilewidth
+        self.tiles = self.load_arena(filename)
+        print(self.tiles)
 
-    n = pygame.Color('lightyellow')
-    g = pygame.Color('purple')
-    b = pygame.Color('blue')
-    y = pygame.Color('yellow')
-    r = pygame.Color('red')
-    x = pygame.Color('black')
-    o = pygame.Color('orange')
-    Tiles = [[b, b, b, y, y, n, n, n, n, n, n, n, n, n, r, o, r, r, r, r],
-             [b, b, y, y, n, n, n, n, n, n, n, n, n, n, n, r, o, r, r, r],
-             [y, y, y, n, n, n, n, n, n, n, n, n, n, n, n, n, r, o, r, r],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, r, o, r],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, r, o],
-             [n, x, x, x, x, x, x, n, n, x, x, n, n, n, n, n, n, n, n, r],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, x, x, x, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, x, x, x, x, x, x],
-             [n, g, n, g, n, g, n, g, n, g, n, g, n, n, x, x, x, x, x, x],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
-             [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n]]
+
+    mapping = {"n": pygame.Color('lightyellow'),
+               "g": pygame.Color('purple'),
+               "b": pygame.Color('blue'),
+               "y": pygame.Color('yellow'),
+               "r": pygame.Color('red'),
+               "x": pygame.Color('black'),
+               "o": pygame.Color('orange')}
+
 
     def draw(self, screen):
-        for i in range(0, len(self.Tiles)):
-            for j in range(0, len(self.Tiles[i])):
-                pygame.draw.rect(screen, self.Tiles[i][j], 
-                                 (j*self.tileheight, 
-                                  i*self.tilewidth, 
-                                  self.tilewidth, 
+        for i in range(0, len(self.tiles)):
+            for j in range(0, len(self.tiles[i])):
+                pygame.draw.rect(screen, self.tiles[i][j],
+                                 (j * self.tileheight,
+                                  i * self.tilewidth,
+                                  self.tilewidth,
                                   self.tileheight))
+
+
+    def load_arena(self, filename):
+        tiles = []
+        with open(filename, 'r') as f:
+            for line in f:
+                tiles.append(list(map(self.mapping.get, list(line.strip()))))
+        return tiles
