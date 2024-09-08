ff628ec48d14605113a5adff80e234ba1a96fd24
Marco <MyDiversity-48048@gmx.de>
2024-09-08 04:56:09 +0200

    fixed the crash issue

--- enemies.py
+++ enemies.py
@@ -23,7 +23,7 @@ class Enemy(pygame.sprite.Sprite):
         # image
         self.frames, self.frame_index = frames, 0
         self.image = self.frames[self.frame_index]
-        self.animation_speed = 0.2
+        self.animation_speed = 0.08
 
         # rect
         self.rect = self.image.get_rect(center=pos)
@@ -38,7 +38,7 @@ class Enemy(pygame.sprite.Sprite):
         # shooting
         self.shooting_dir = pygame.math.Vector2()
         self.last_shot_time = pygame.time.get_ticks()
-        self.shoot_cooldown = 500
+        self.shoot_cooldown = 2500
 
     def animate(self):
         self.frame_index += self.animation_speed
@@ -80,6 +80,8 @@ class Enemy(pygame.sprite.Sprite):
         current_time = pygame.time.get_ticks()
         if current_time - self.last_shot_time >= self.shoot_cooldown:
             pos = self.rect.center
+            self.image = pygame.image.load("img/shot/1.png")
+            self.image = pygame.image.load("img/shot/2.png")
             Bullet(player_bullet_surf, pos,
                    self.shooting_dir,
                    (self.all_sprites, self.bullet_sprites))
--- main.py
+++ main.py
@@ -222,6 +222,7 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 game_active = False
                 settings_active = False
+        
         # handler for enemy spawn event
         if event.type == enemy_event:
             Enemy(choice(Wasteland_arena.spawn_positions),
@@ -252,17 +253,17 @@ while True:
         # bullet collisions
         if bullet_sprites:
             for bullet in bullet_sprites:
-                collision_sprites = pygame.sprite.spritecollide(
+                hit_sprites = pygame.sprite.spritecollide(
                     bullet, enemy_sprites, dokill=True,
                     collided=pygame.sprite.collide_mask)
-                if collision_sprites:
-                    for sprite in collision_sprites:
+                if hit_sprites:
+                    for sprite in hit_sprites:
                         sprite.destroy()
                         enemy_sprites.remove(sprite)  # Remove from group
                         bullet.kill()  # Remove the bullet
                         score += 1
                         enemy_destroyed_sound.play()
-
+    
         if enemy_bullet_sprites:
             # check if player is hit by enemy bullet
             player_hit = pygame.sprite.spritecollide(
